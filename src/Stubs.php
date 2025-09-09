2ded2dc8f288ccc5c144d127cfd0eb03ca362b3c
Jan Matthiesen <jm@netinventors.de>
2025-09-09 12:33:18 +0200

    Adds Symfony Finder to iterate stubs
    Uses Symfony Finder component to iterate over stub files.
    This change improves the performance of the plugin and makes
    it more reliable.
    Removes composer.lock and adds it to .gitignore

--- src/Stubs.php
+++ src/Stubs.php
@@ -6,13 +6,21 @@ namespace NetInventors\Shopware6PsalmStubs;
 
 use Psalm\Plugin\PluginEntryPointInterface;
 use Psalm\Plugin\RegistrationInterface;
+use Symfony\Component\Finder\Finder;
 
 class Stubs implements PluginEntryPointInterface
 {
     public function __invoke(RegistrationInterface $registration, \SimpleXMLElement|null $config = null): void
     {
-        foreach (\glob(__DIR__ . '/../stubs/**/*.phpstub') as $stub) {
-            $registration->addStubFile($stub);
+        $finder = new Finder();
+
+        $finder->files()
+            ->in(__DIR__ . '/../stubs')
+            ->name('*.stub.php')
+        ;
+
+        foreach ($finder as $stub) {
+            $registration->addStubFile($stub->getRealPath());
         }
     }
 }
